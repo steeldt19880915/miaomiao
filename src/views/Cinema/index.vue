0a05e50b054fe21e303dcfa7f04b4d28afdb9eb1
steeldt19880915 <stallonedll@sina.com>
2019-12-09 16:11:38 +0800

    创建tabbar三大模块、配路由、安装插件

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello cineam
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello mine
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
